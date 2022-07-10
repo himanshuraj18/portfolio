edd67cbfaf61a0d16be59f8876ad867d44c3264b
Himanshu Raj <himanshu18038@iiitd.ac.in>
2022-07-11 01:58:21 +0530

    Minor BugFix

--- components/Banner.js
+++ components/Banner.js
@@ -4,10 +4,10 @@ import { socialsList } from '/components/Lists'
 export default function Banner() {
     return (
         <div className="w-full h-screen flex flex-col grid grid-cols-1 sm:grid-cols-2">
-            <div className="col-span-1 flex flex-col mx-auto sm:mx-4 mt-auto sm:mt-0 mb-8 sm:mb-0">
+            <div className="col-span-1 flex flex-col mx-auto sm:mx-4 mt-auto sm:my-auto mb-8 sm:my-auto">
                 <img className="w-64 sm:w-80 h-64 sm:h-80 ml-0 sm:ml-auto object-cover rounded-lg border-4 border-pink-500 shadow-2xl" src="assets/profile_pic.jpg" />
             </div>
-            <div className="col-span-1 flex flex-col mx-auto sm:mx-4 mb-auto sm:mb-0 mt-8 sm:mt-0">
+            <div className="col-span-1 flex flex-col mx-auto sm:mx-4 mb-auto sm:my-auto mt-8 sm:my-auto">
                 <span className="text-4xl sm:text-7xl font-medium text-center sm:text-left">
                     HIMANSHU RAJ
                 </span>
