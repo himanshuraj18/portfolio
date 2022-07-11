17bbed185de000fa64a239449743b3dacd71ea74
Himanshu Raj <himanshu18038@iiitd.ac.in>
2022-07-11 17:52:12 +0530

    Title and Favicon Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import '/public/customStyle.css';
+import Head from 'next/head';
 // import Header from '/components/Header';
 import Footer from '/components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="flex flex-col scrollbar-thin scrollbar-track-transparent scrollbar-thumb bg-gradient-to-r from-green-200 to-purple-300 background-animate">
+      <Head>
+        <title>Himanshu's Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      </Head>
       {/* <Header/> */}
       <Component {...pageProps} />
       <Footer/>
