42bbc75a2bd37b412afce09f96509044e354b7fa
Himanshu Raj <himanshu18038@iiitd.ac.in>
2022-07-11 17:27:39 +0530

    BugFix

--- components/About.js
+++ components/About.js
@@ -2,8 +2,8 @@ export default function About(props) {
     return (
         <div className="flex bg-white bg-opacity-40 backdrop-blur-md drop-shadow-lg rounded-lg my-4 mx-auto">
             <span className="m-auto p-8">
-                I am a passionate software developer and I've just completed my bachelor degree from IIIT-Delhi. 
-                I've received offers from Google Dublin and Microsoft India after my undergraduate degree. I have
+                I am a passionate software developer and I&apos;ve just completed my bachelor degree from IIIT-Delhi. 
+                I&apos;ve received offers from Google Dublin and Microsoft India after my undergraduate degree. I have
                 worked on various projects in areas of Software Development and Machine Learning.<br/><br/>
                 Apart from tech, I am an avid follower (and an amateur player) of chess and cricket. You can also
                 find me enjoying songs or sleeping or both :D
